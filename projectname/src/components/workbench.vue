a3b2757eb084bb431aa1a73fad8a27c93de05973
ljj <ljj@302d9d71-1efd-4c97-91d4-3b672fdcfd1f>
2018-01-11 07:44:53 +0000

    打包后图片路径错误修改

--- projectname/src/components/workbench.vue
+++ projectname/src/components/workbench.vue
@@ -16,31 +16,31 @@
                     child: [{
                         text: '校区管理',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/01@2x.png'
+                        imgSrc: require('../../static/images/gzt/01@2x.png')
                     }, {
                         text: '班级管理',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/02@2x.png'
+                        imgSrc: require('../../static/images/gzt/02@2x.png')
                     }, {
                         text: '教室管理',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/03@2x.png'
+                        imgSrc: require('../../static/images/gzt/03@2x.png')
                     }, {
                         text: '教师管理',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/04@2x.png'
+                        imgSrc: require('../../static/images/gzt/04@2x.png')
                     }, {
                         text: '课程管理',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/05@2x.png'
+                        imgSrc: require('../../static/images/gzt/05@2x.png')
                     }, {
                         text: '缴费提醒',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/06@2x.png'
+                        imgSrc: require('../../static/images/gzt/06@2x.png')
                     }, {
                         text: '健康管理',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/07@2x.png'
+                        imgSrc: require('../../static/images/gzt/07@2x.png')
                     }]
                 },
                 workData2: {
@@ -48,27 +48,27 @@
                     child: [{
                         text: '午托点名',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/08@2x.png'
+                        imgSrc: require('../../static/images/gzt/08@2x.png')
                     }, {
                         text: '晚托点名',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/09@2x.png'
+                        imgSrc: require('../../static/images/gzt/09@2x.png')
                     }, {
                         text: '课程点名',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/10@2x.png'
+                        imgSrc: require('../../static/images/gzt/10@2x.png')
                     }, {
                         text: '食谱记录',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/11@2x.png'
+                        imgSrc: require('../../static/images/gzt/11@2x.png')
                     }, {
                         text: '作业点评',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/12@2x.png'
+                        imgSrc: require('../../static/images/gzt/12@2x.png')
                     }, {
                         text: '食谱记录',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/13@2x.png'
+                        imgSrc: require('../../static/images/gzt/13@2x.png')
                     }]
                 },
                 workData3: {
@@ -76,11 +76,11 @@
                     child: [{
                         text: '到校情况',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/14@2x.png'
+                        imgSrc: require('../../static/images/gzt/14@2x.png')
                     }, {
                         text: '学生表现',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/15@2x.png'
+                        imgSrc:require( '../../static/images/gzt/15@2x.png')
                     }, ]
                 },
                 workData4: {
@@ -88,19 +88,19 @@
                     child: [{
                         text: '安全定位',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/16@2x.png'
+                        imgSrc: require('../../static/images/gzt/16@2x.png')
                     }, {
                         text: '视频监控',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/17@2x.png'
+                        imgSrc: require('../../static/images/gzt/17@2x.png')
                     }, {
                         text: '积分管理',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/18@2x.png'
+                        imgSrc: require('../../static/images/gzt/18@2x.png')
                     }, {
                         text: '商城',
                         href: '/xqgl',
-                        imgSrc: '../../static/images/gzt/19@2x.png'
+                        imgSrc: require('../../static/images/gzt/19@2x.png')
                     }]
                 }
             }
