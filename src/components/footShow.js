968df2a6625f5fe7add0c30141e9ffcd687d3514
ljj <ljj@302d9d71-1efd-4c97-91d4-3b672fdcfd1f>
2018-01-15 07:40:27 +0000

    删除旧的

--- src/components/footShow.js
+++
@@ -1,7 +0,0 @@
-import footShowComponent from './footShow.vue'
-const footShow = {
-    install: function(Vue) {
-        Vue.component('footShow', footShowComponent);
-    }
-}
-export default footShow
--- src/components/workbench/index.js
+++
@@ -1,7 +0,0 @@
-import workComponent from './workComponent.vue'
-const  workbench = {
-    install:function(Vue){
-        Vue.component('workbench',workComponent);
-    }
-}
-export default workbench
