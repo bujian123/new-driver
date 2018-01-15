3d556359276a283f440f38374ae26cd1c512123d
ljj <ljj@302d9d71-1efd-4c97-91d4-3b672fdcfd1f>
2018-01-15 07:48:08 +0000

    业务操作VUE

---
+++ src/components/home/operations.vue
@@ -0,0 +1,19 @@
+
+<!-- 业务操作 -->
+<template>
+        <div>
+            <div>业务操作</div>
+        </div>
+    </template>
+    <script>
+        export default {
+            data(){
+                return{
+                    
+                }
+            }
+        }
+    </script>
+    <style>
+    
+    </style>
