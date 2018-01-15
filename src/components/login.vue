92a00a5f6666eec4b987d0cec1932d8c187abe54
ljj <ljj@302d9d71-1efd-4c97-91d4-3b672fdcfd1f>
2018-01-15 09:29:07 +0000

    登陆瞎写

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,14 +1,22 @@
 <!-- 登陆 -->
 <template>
     <div>
-
+        <img src="../../static/images/mine/3@2x.png" />
+        <form>
+                <mt-field label="账号" placeholder="请输入账号" v-model="userName"></mt-field>
+                <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>    
+                <div style="display:flex;justify-content:center">  <mt-button size="large" style="width:50%">large</mt-button>  </div>   
+        </form>
+          
+          
     </div>
 </template>
 <script>
     export default{
         data(){
             return {
-
+                userName:'',
+                password:''
             }
         }
     }
