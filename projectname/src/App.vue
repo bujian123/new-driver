b55aa01d16715dcd154f2f781c1c23445e49536b
ctf <ctf@302d9d71-1efd-4c97-91d4-3b672fdcfd1f>
2018-01-15 15:55:26 +0000

    dasfdfdas

--- projectname/src/App.vue
+++ projectname/src/App.vue
@@ -53,14 +53,18 @@
   html {
     margin: 0;
     padding: 0;
+    height: 100%;width: 100%;
   }
-
+  *{
+    font-size: 0.3rem;
+  }
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    height: 100%;width: 100%;
   }
 
   header {
